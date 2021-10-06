e214d0242592079065a0f763551fccac271e3bbd
danny vincent <87720409+1oupd@users.noreply.github.com>
2021-10-06 14:50:05 +0100

    added base js code

---
+++ app.js
@@ -0,0 +1,41 @@
+// keydown
+
+document.addEventListener("keydown", (event) => {
+  if (event.code === KeyA) {
+    //add audio
+  } else if (event.code === KeyS) {
+    //add audio
+  } else if (event.code === KeyD) {
+    //add audio
+  } else if (event.code === KeyF) {
+    //add audio
+  } else if (event.code === KeyG) {
+    //add audio
+  } else if (event.code === KeyH) {
+    //add audio
+  } else if (event.code === KeyI) {
+    //add audio
+  } else if (event.code === KeyJ) {
+    //add audio
+  } else if (event.code === KeyK) {
+    //add audio
+  } else if (event.code === KeyL) {
+    //add audio
+  } else {
+      // ?
+  }
+});
+
+// click
+
+document.addEventListener("click", (event) => {
+  let boom = document.getElementById("boom");
+  let clap = document.getElementById("clap");
+  let hihat = document.getElementById("hihat");
+  let kick = document.getElementById("kick");
+  let openhat = document.getElementById("openhat");
+  let ride = document.getElementById("ride");
+  let snare = document.getElementById("snare");
+  let tink = document.getElementById("tink");
+  let tom = document.getElementById("tom");
+});
